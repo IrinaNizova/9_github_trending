f2f134334612934ce7503cf3f0af96f4826a9677
Ira <you@example.com>
2017-10-17 23:32:55 +0300

    create task

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Github Trends
 
-[TODO. There will be project description]
+With this script you can look 20 most starring github projects for the last week and its bugs
+
+## Example of  script launch on Linux, Python 3.5:
+```$ python github_trending.py```
+
+## Example of output
+```json------------------------------
+Name: trumail
+Description: :email: :white_check_mark: A Fast and Free Email Verification API written in Go
+You can look it for address https://github.com/sdwolfe32/trumail
+But this project has 2 bugs
+Bug title: Minimum Length E-Mail Create at 2017-10-17T13:57:39Z
+Bug title: Email provider support Create at 2017-10-17T07:19:08Z
+------------------------------
+Name: atomic-red-team
+Description: Small and highly portable detection tests.
+You can look it for address https://github.com/redcanaryco/atomic-red-team
+But this project has 1 bugs
+Bug title: UAC Bypass Create at 2017-10-12T14:48:46Z
+------------------------------
+Name: git-bomb
+Description: A (nearly) uncloneable repo
+You can look it for address https://github.com/Katee/git-bomb
+This project have not bugs!
+------------------------------```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,52 @@
+import requests
+from datetime import date, timedelta
+
+REPOS_NUMBER = 20
+
+
+def get_github_api_host():
+    return "https://api.github.com"
 
 
 def get_trending_repositories(top_size):
-    pass
+    params = {'q': 'created:>{}'.format(date.today() - timedelta(days=7)),
+              'sort': 'stars', 'per_page': top_size, 'order': 'desc'}
+    repositories = requests.get("/".join((get_github_api_host(), 'search', 'repositories')), params=params)
+    if repositories.status_code != 200:
+        return None
+    return repositories.json()['items']
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    try:
+        issues = requests.get("/".join((get_github_api_host(), 'repos', repo_owner, repo_name, 'issues')))
+        print("/".join((get_github_api_host(), 'repos', repo_owner, repo_name, 'issues')))
+        return [bug for bug in issues.json() if bug['state'] == 'open']
+    except (requests.exceptions.ConnectionError, TypeError):
+        return []
+
+
+def print_repositories_info(repo_data):
+    print("Here are {} most popular repositories for last week:".format(len(repo_data)))
+    for repository in repo_data:
+        print("-" * 30)
+        print("Name: {}".format(repository['name']))
+        print("Description: {}".format(repository['description']))
+        print("You can look it for address {}".format(repository['html_url']))
+        if int(repository["open_issues_count"]) > 0:
+            print("But this project has {} bugs".format(repository["open_issues_count"]))
+            bugs_data = get_open_issues_amount(repository['owner']['login'], repository['name'])
+            for bug in bugs_data:
+                print("Bug title: {} Create at {}".format(bug['title'], bug['created_at']))
+            if not bugs_data:
+                print("We could not get information about bugs")
+        else:
+            print("This project have not bugs!")
+
 
 if __name__ == '__main__':
-    pass
+    repositories = get_trending_repositories(REPOS_NUMBER)
+    if repositories:
+        print_repositories_info(repositories)
+    else:
+        print("Unfortunately, github is not available now")
